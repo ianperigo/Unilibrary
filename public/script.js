17086709f211cc41366e0b0802ef1feaa02c1f0f
IanPerigoUERN <ian20240027015@alu.uern.br>
2025-06-08 20:18:45 -0300

    Atualizando o script.js para o sevidor do back

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_URL = 'http://localhost:3000';
+    const API_URL = 'https://unilibrary-production.up.railway.app/';
 
     // Formulários
     const registerForm = document.getElementById('register-form');
